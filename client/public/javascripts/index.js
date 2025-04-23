12cbfcc3466ae9d6fcd6b5363f28ebc757d040b7
h4wks123 <ivanne_bayer@LAPTOP-31P56QLH.>
2025-04-23 21:51:02 +0800

    feat: added ejs todo list boilerplate with nodejs backend

---
+++ client/public/javascripts/index.js
@@ -0,0 +1,6 @@
+createTodoList = () => {
+  let inputValue = document.getElementById("todo-list-header__input").value;
+  document.getElementById("todo-list-header__input").value = "";
+
+  console.log(inputValue);
+};
---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.set("view engine", "ejs");
+app.set("views", "./client/views");
+
+// Allows static configuration for public files
+app.use(express.static(__dirname.replace("/server", "") + "/client/public"));
+
+// Renders view page from ejs file
+app.get("/", (req, res) => {
+  res.render("login", { nigger: "Hello World!" });
+});
+
+// Runs server with port number
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
